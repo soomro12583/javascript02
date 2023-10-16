d1d76ad5960c3051ba9e135bad5b6c2844167492
soomro12583 <nadeemkhanasf909@gmail.com>
2023-10-16 23:31:24 +0500

    1st Question

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+//const sum = num1 - num2;
+//const sum = num1 * num2;
+//const sum = num1 / num2;
+
+// display the sum
+document.write(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+
+
