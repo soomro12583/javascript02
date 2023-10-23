ec8f1758adda9813da87731e5fc5960161c8e638
soomro12583 <nadeemkhanasf909@gmail.com>
2023-10-23 07:30:48 +0500

    changes

--- app.js
+++ app.js
@@ -6,40 +6,41 @@ const num2 = parseInt(prompt('Enter the second number '));
 //add two numbers
 const sum = num1 + num2;
 
-document.write(`The sum of ${num1} and ${num2} is ${sum}`);
+document.write(`<h1>The sum of ${num1} and ${num2} is ${sum}</h1>`);*/
 
 
 //document.write(`Question 4:`)
-const tkt =parseInt(prompt("total price of ticket RS.600/-" ));
-document.write(`<center>Total cost of <b>${tkt}</b> tickets is : <b>${tkt*600}</b> PKR.</center>`);
+//const tkt =parseInt(prompt("total price of ticket RS.600/-" ));
+//document.write(`<h1><center>Total cost of <b>${tkt}</b> tickets is : <b>${tkt*600}</b> PKR.</center></h1>`);
 
-let a = 7;
+/*let a = 7;
 let b=2;
+let sum =b+a;
 let sub = a-b;
 let Multiplay = a*b;
 let divide = a/b;
 let modulus = a%b;
 document.write(`Question 02<br><br>`);
+document.write(`sum of ${b} and ${a} is: ${sum} <br>`);
 
-document.write(`Sum of ${a} and ${b} is: ${sum}<br>`);
 document.write(`Subtract of ${a} and ${b} is: ${sub}<br>`);
 document.write(`Multiply of ${a} and ${b} is: ${Multiplay}<br>`);
 document.write(`Division of ${a} and ${b} is: ${divide}<br>`);
-document.write(`Modulus of ${a} and ${b} is: ${modulus}`);
+document.write(`Modulus of ${a} and ${b} is: ${modulus}`);*/
 
 
-document.write(`<br><br>Question 03<br>`);
+/*document.write(`<br><br>Question 03<br>`);
 document.write(`Value after variable declaration is: Undefine.<br>`);
 document.write(`Initial value: is: ${a}.<br>`);
 document.write(`Value after increment is: ${b}.<br>`);
 document.write(`Value after decrement is: ${sub}.<br>`);
-document.write(`The remainder is : ${modulus}.<br><br>`);
+document.write(`The remainder is : ${modulus}.<br><br>`);*/
 
-document.write(`<center><h2>Table of 4</h2>`)
+/*document.write(`<center><h2>Table of 4</h2>`)
  for(var i=1;i<11;i++){
     var tablenum=4;
     document.write(`<center><h4>${tablenum} X ${i} = ${tablenum*i}</h4>`)
- }
+ }*/
 
  
 
@@ -51,8 +52,35 @@ document.write(`<center><h2>Table of 4</h2>`)
  let c=(forn-32)*5/9
  document.write(`${forn} °F is ${c} °C`)*/
 
- var x = prompt("what is your name?");
+ /*var x = prompt("what is your name?");
 if (x === "nadeem");{
-   alert("correct!");}
+   alert("correct!");}*/
+
+   //console.log("Muhammad nadeem Soomro");
+   
+   /*var x = prompt("where do you live?");
+   if (x === "shikarpur"){
+   document.write("good these are equal.");
+   }else{
+      document.write("not equal");
+   }
+   if (x === "sindh"){
+      document.write("not province type city name");
+   }*/
+
+   var a = 7;
+   var b = 8;
+   var sum = a + b;
+   console.log(sum);
+   let sub = a-b;
+   let multiply = a*b;
+   var divide = b/a;
+   console.log(`sum of ${a} and ${b} is : ${sum}`)
+   console.log(`Minus of ${a} and ${b} is : ${sub}`);
+   console.log(`multiply of ${a} and ${b} is : ${multiply}`);
+   console.log(`division of ${a} and ${b} is : ${divide}`);
+
+   
+
    
 
